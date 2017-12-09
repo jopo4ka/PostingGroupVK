89cafa40fc1d00aa1fb243d9919e78e0fe8b5509
jopo4ka <sobakasobaka6@gmail.com>
2017-12-09 20:05:54 +0500

    fix group

--- PostingGroupVK/Form1.Designer.cs
+++ PostingGroupVK/Form1.Designer.cs
@@ -28,12 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSrch = new System.Windows.Forms.TextBox();
+            this.btnSnd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.srchBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.albumID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.membersCnt = new System.Windows.Forms.NumericUpDown();
+            this.msgWall = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.membersCnt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ЛОГИН";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ПАРОЛЬ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(234, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(431, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Состояние";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSrch
+            // 
+            this.txtSrch.Location = new System.Drawing.Point(179, 41);
+            this.txtSrch.Name = "txtSrch";
+            this.txtSrch.Size = new System.Drawing.Size(246, 20);
+            this.txtSrch.TabIndex = 1;
+            // 
+            // btnSnd
+            // 
+            this.btnSnd.Location = new System.Drawing.Point(179, 276);
+            this.btnSnd.Name = "btnSnd";
+            this.btnSnd.Size = new System.Drawing.Size(327, 23);
+            this.btnSnd.TabIndex = 3;
+            this.btnSnd.Text = "Sending";
+            this.btnSnd.UseVisualStyleBackColor = true;
+            this.btnSnd.Click += new System.EventHandler(this.btnSnd_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(494, 178);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // srchBtn
+            // 
+            this.srchBtn.Location = new System.Drawing.Point(431, 39);
+            this.srchBtn.Name = "srchBtn";
+            this.srchBtn.Size = new System.Drawing.Size(75, 23);
+            this.srchBtn.TabIndex = 3;
+            this.srchBtn.Text = "Search";
+            this.srchBtn.UseVisualStyleBackColor = true;
+            this.srchBtn.Click += new System.EventHandler(this.btnSrch_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "ALBUM";
+            // 
+            // albumID
+            // 
+            this.albumID.Location = new System.Drawing.Point(63, 276);
+            this.albumID.Name = "albumID";
+            this.albumID.Size = new System.Drawing.Size(100, 20);
+            this.albumID.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Участн.";
+            // 
+            // membersCnt
+            // 
+            this.membersCnt.Location = new System.Drawing.Point(64, 42);
+            this.membersCnt.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.membersCnt.Name = "membersCnt";
+            this.membersCnt.Size = new System.Drawing.Size(99, 20);
+            this.membersCnt.TabIndex = 5;
+            // 
+            // msgWall
+            // 
+            this.msgWall.Location = new System.Drawing.Point(12, 250);
+            this.msgWall.Name = "msgWall";
+            this.msgWall.Size = new System.Drawing.Size(494, 20);
+            this.msgWall.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 309);
+            this.Controls.Add(this.msgWall);
+            this.Controls.Add(this.membersCnt);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSnd);
+            this.Controls.Add(this.srchBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtSrch);
+            this.Controls.Add(this.albumID);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.membersCnt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSrch;
+        private System.Windows.Forms.Button btnSnd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button srchBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox albumID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown membersCnt;
+        private System.Windows.Forms.TextBox msgWall;
     }
 }
 
--- PostingGroupVK/Form1.cs
+++ PostingGroupVK/Form1.cs
@@ -2,19 +2,174 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
 
 namespace PostingGroupVK
 {
     public partial class Form1 : Form
     {
+        public VkApi vk = new VkApi();
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = textBox1.Text;        // email или телефон
+            string pass = textBox2.Text;         // пароль для авторизации
+            Settings scope = Settings.All;      // Приложение имеет доступ к друзьям
+            vk.Authorize(new ApiAuthParams
+            {
+                ApplicationId = 6116967,
+                Login = email,
+                Password = pass,
+                Settings = scope
+            });
+            if (vk.IsAuthorized)
+            {
+                label3.Text = "AUTHORISED";
+            }
+            else
+            {
+                label3.Text = "NONE";
+            }
+        }
+
+        private void btnSrch_Click(object sender, EventArgs e)
+        {
+            if (vk.IsAuthorized)
+            {
+                int cnt = 0;
+                int totalCnt;
+                var groups = vk.Groups.Search(out totalCnt, new GroupsSearchParams{ Query = txtSrch.Text, Count=100 });
+                foreach (var group in groups)
+                {
+                    var getted = vk.Groups.GetById(group.Id, GroupsFields.CanPost | GroupsFields.CanSeelAllPosts | GroupsFields.MembersCount);
+                    bool canPost = getted.CanPost;
+                    bool pblcGrp = getted.CanSeelAllPosts;
+                    bool valid;
+                    if (getted.MembersCount > membersCnt.Value){ valid = true; }
+                                              else { valid = false; }
+                    if (canPost && pblcGrp && valid)
+                    {
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[cnt].Cells[0].Value = group.Name;
+                        dataGridView1.Rows[cnt].Cells[1].Value = canPost;
+                        dataGridView1.Rows[cnt].Cells[2].Value = group.Id;
+                        try
+                        {
+                            PhotoGetAlbumsParams pgap = new PhotoGetAlbumsParams
+                            {
+                                OwnerId = 0 - group.Id,
+
+                            };
+                            var albums = vk.Photo.GetAlbums(pgap);
+                            int albCnt = 0;
+                            foreach (var album in albums)
+                            {
+                                DataGridViewTextBoxColumn secondColumn = new DataGridViewTextBoxColumn();
+                                secondColumn.HeaderText = "Name " + albCnt;
+                                secondColumn.Name = albCnt.ToString();
+                                if (dataGridView1.ColumnCount <= albCnt + 3)
+                                {
+                                    dataGridView1.Columns.Add(secondColumn);
+                                }
+                                dataGridView1.Rows[cnt].Cells[albCnt + 3].Value = album.Id;  //.Value = album.Id;
+                                albCnt++;
+                            }
+                        }
+                        catch 
+                        {
+                            Debugger.Log(1, "Exception", "Exception");
+                        }
+                        finally {
+                            cnt++;
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        private void btnSnd_Click(object sender, EventArgs e)
+        {
+            string[] rf = new string[4];
+            var wc = new WebClient();
+
+            if (dataGridView1.ColumnCount > 3)
+            {
+                for (int row = 0; dataGridView1.RowCount - 1 > row; row++)
+                {
+                    var groupId = long.Parse(dataGridView1.Rows[row].Cells[2].Value.ToString());
+                      
+                      var photos = vk.Photo.Get(new PhotoGetParams
+                      {
+                          Count = 4,
+                          AlbumId = PhotoAlbumType.Id(int.Parse(albumID.Text)),
+                          OwnerId = vk.UserId
+                      });
+
+                    WallPostParams wpp = new WallPostParams
+                    {
+                        Message = msgWall.Text,
+                        OwnerId = 0-groupId,
+                        Attachments = photos 
+                      };  
+                      vk.Wall.Post(wpp);
+
+                    //
+
+                    for (int cel = 3; dataGridView1.ColumnCount - 1 > cel && dataGridView1.Rows[row].Cells[cel].Value != null; cel++)
+                    {
+                        try { 
+                            long albumId = long.Parse(dataGridView1.Rows[row].Cells[cel].Value.ToString());
+                            var ups = vk.Photo.GetUploadServer(albumId, groupId);
+                            rf[0] = Encoding.ASCII.GetString(wc.UploadFile(ups.UploadUrl, System.Windows.Forms.Application.StartupPath + @"\file1.jpg"));
+                            rf[1] = Encoding.ASCII.GetString(wc.UploadFile(ups.UploadUrl, System.Windows.Forms.Application.StartupPath + @"\file2.jpg"));
+                            rf[2] = Encoding.ASCII.GetString(wc.UploadFile(ups.UploadUrl, System.Windows.Forms.Application.StartupPath + @"\file3.jpg"));
+                            rf[3] = Encoding.ASCII.GetString(wc.UploadFile(ups.UploadUrl, System.Windows.Forms.Application.StartupPath + @"\file4.jpg"));
+                            for (int i = 0; i < 4; i++)
+                            {
+                                var sPhotos = vk.Photo.Save(new PhotoSaveParams
+                                {
+                                    SaveFileResponse = rf[i],
+                                    GroupId = groupId,
+                                    AlbumId = albumId
+                                });
+                            }
+                        }   
+                        catch
+                        {
+                            Debugger.Log(1, "Exception", "Exception");
+                        }
+                    }
+                }
+               
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DateTime date1 = new DateTime(2917, 10, 7, 7, 00, 00);
+            DateTime nowDate = DateTime.Now;
+            if (date1 <= nowDate)
+            {
+                this.Close();
+            }
         }
     }
 }
